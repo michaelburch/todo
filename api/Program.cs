ede12464f69aaa1328a1aab6e1f89e57eed181cb
Michael Burch <michaelburch@outlook.com>
2020-05-08 10:36:26 -0500

    lax cors policy

--- api/Program.cs
+++ api/Program.cs
@@ -14,8 +14,15 @@ namespace todo
             var builder = WebApplication.CreateBuilder(args);
             
             builder.Services.AddDbContext<TodoDbContext>();
-            
+            builder.Services.AddCors(options => 
+            {
+                options.AddDefaultPolicy( 
+                cb => { cb.AllowAnyOrigin(); cb.AllowAnyMethod();}
+                );
+            });
+         
             var app = builder.Build();
+            app.UseCors();
             
             var todos = new TodoApi();
             todos.MapRoutes(app);
